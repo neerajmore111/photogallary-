c9890454c510a190da58654ac3924b62c8b00754
neerajmore111 <153122905+neerajmore111@users.noreply.github.com>
2025-05-26 17:37:54 +0530

    Update main.jsx
    d

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'
+import App from './Photogallary.jsx'
 import { BrowserRouter } from 'react-router'
 import { Provider } from 'react-redux'
 import { store } from './REdux/store.js'
